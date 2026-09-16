14ffaeb83b6f2b907cce3490483b23361e3720e9
Sumeet Attri <https://github.com/SumeetAttri09>
2026-09-16 23:30:00 +0530

    type conversion

--- code.py
+++ code.py
@@ -9,16 +9,21 @@ print("my name is", name, "and I am ",age, "years old")'''
 #total_price = 100
 
 #sum of two numbers
-a=10
-b=20
-sum = a+b
-mul = a*b
-print(sum)
-print(a<b)
-print(a!=b)
+# a=10
+# b=20
+# sum = a+b
+# mul = a*b
+# print(sum)
+# print(a<b)
+# print(a!=b)
 
-a-=5
-print(a)
-b+=5
-print(b)
+# a-=5
+# print(a)
+# b+=5
+# print(b)
 
+
+ans1 = int(5+10.0)
+ans2 = 5+10.0
+print(ans1, type(ans1))
+print(ans2, type(ans2))
